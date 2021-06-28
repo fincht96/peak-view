2978d1046ee6520c79a518ab7b5c615b9206128d
Tom <fincht96@gmail.com>
2021-06-28 08:51:43 +0100

    Added support for querying PostgreSQL db from Express app

--- server/index.js
+++ server/index.js
@@ -3,6 +3,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'tom',
+  host: 'database',
+  database: 'pgdb',
+  password: 'Lemon501',
+  port: 5432,
+})
+pool.query(`SELECT * FROM color_table;`, (err, res) => {
+  console.log("Here!")
+
+  if(err){
+      console.log("error: ", err)
+  }
+  else{
+      console.log(res)
+  }
+
+  pool.end()
+})
+// const client = new Client({
+//   user: 'dbuser',
+//   host: 'database.server.com',
+//   database: 'mydb',
+//   password: 'secretpassword',
+//   port: 3211,
+// })
+// client.connect()
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
 
 
 app.get('/', (req, res) => {
